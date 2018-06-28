66d02f32e6ee14a01a3bac83bbc2abaa49d74fc8
latajatav <40224403+latajatav@users.noreply.github.com>
2018-06-28 05:42:17 +0530

    test

---
+++ test.py
@@ -0,0 +1,44 @@
+#print("Hello python")
+
+#a,b = " Hello"," world "
+#print(a,b + "what'sapp")
+
+#a,b = "Hello","There"
+#print(a+" World "+b)
+
+#A = " Hey There "
+#print(A)
+#print(A[1:])
+#print(A[1:5])
+#print(A+2)
+#print(A + "Emotech")
+
+# 3list in python
+
+#str="Hello world"
+#print(len(str))
+
+#len()function returns the length of a string
+#str="Hello world"
+#print(len(str)-4)
+#print(len('Hey'))
+
+#using array
+#A=[10,20,30,40]
+#print(A)
+#print(A[1])
+
+#A=[10,2.5,"Hey",1,2,3]
+#print(A)
+#print(A[2])
+
+#mylist=(10,"abc",2.5,'batman')
+#print(mylist[1:])
+#print(mylist*5)
+#error program
+#mylist2=["Dog",15]
+#Newlist =mylist + mylist2
+#print(Newlist))
+#print(mylist[0]+4)
+
+A,B=
