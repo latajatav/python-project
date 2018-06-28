5ad66880aa9fb5e4c901682d3c7e844be2ec9c9a
latajatav <40224403+latajatav@users.noreply.github.com>
2018-06-28 21:25:06 +0530

    list function

---
+++ testing.py
@@ -0,0 +1,10 @@
+#a=[10,20,30,40,50,60,70]
+#a.insert(len(a)-3,80)
+#print(a)
+students = ["rahul", "neeraj", "lata", "hema", "surbhi", "arvind"]
+print(students[0])
+print(students[1])
+students.append("amit")
+print(students)
+students[3]="pihu"
+print(students)
