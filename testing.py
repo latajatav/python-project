0ba74f8685e44e8ed6b0f66b329703e82264e479
latajatav <40224403+latajatav@users.noreply.github.com>
2018-07-02 22:18:43 +0530

    Add files via upload
    class creation

--- testing.py
+++ testing.py
@@ -1,10 +1,47 @@
 #a=[10,20,30,40,50,60,70]
 #a.insert(len(a)-3,80)
 #print(a)
-students = ["rahul", "neeraj", "lata", "hema", "surbhi", "arvind"]
-print(students[0])
-print(students[1])
-students.append("amit")
-print(students)
-students[3]="pihu"
-print(students)
+#students = ["rahul", "neeraj", "lata", "hema", "surbhi", "arvind"]
+#print(students[0])
+#print(students[1])
+#print(students)
+#students[3]="pihu"
+#print(students)
+
+
+
+
+#class Student :
+#     name=' '
+#     rollnumber=' 5'
+#     year='2'
+#     collage=' '
+#     def dispall (self):
+#         print(self.name)
+#         print(self.rollnumber)
+#         print(self.year)
+#     s1 = student()
+#     s1.student=input('enter name:')
+#     s1.rollnumber=int(input('enter rollnumber:'))
+
+#     s1.year=int(input("enter the year:"))
+#     s1.collage=input("enter collage")
+#     s1.dispall()
+#     s1=student()#instantistion of class student
+
+
+#class sample:
+#def __int__(self,a,b,c):
+          #self.v1=a
+          #self.v2=b
+          #self.v3=c
+#s1=sample(10,'ram',3.20)
+#print(s1.v1)
+#print(s1.v2)
+#print(s1.v3)
+
+
+
+
+
+ 
